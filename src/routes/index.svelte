b5b7414ba795bf8df1d4d11aff5d31527333d188
mudiageo <76590594+mudiageo@users.noreply.github.com>
2022-02-28 20:31:44 +0000

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -84,9 +84,36 @@ let params = Object.entries(data).map(([key, val]) => `${key}=${encodeURICompone
       </ul>
     </div>
   </div>
-  <div class="bottom-0 flex items-center justify-between w-full p-3 border-t border-gray-300">
-  <input class="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700" placeholder="Message" name="message" on:keyup={localStorage.setItem('newMessage', newMessage)} bind:value={newMessage} required type="text">
-  <button class="btn btn-outline" type="submit">Submit</button>
+
+    <!-- MESSAGE INPUT AREA -->
+    <div class="fixed w-full flex justify-between bg-green-100" style="bottom: 0px;">
+      <textarea
+        class="flex-grow m-2 py-2 px-4 mr-1 rounded-full border border-gray-300 bg-gray-200 resize-none"
+        rows="1"
+on:keyup={localStorage.setItem('newMessage', newMessage)}
+bind:value={newMessage}
+        placeholder="Message..."
+        style="outline: none;"
+      ></textarea>
+      <button class="m-2" style="outline: none;">
+        <svg
+          class="svg-inline--fa text-green-400 fa-paper-plane fa-w-16 w-12 h-12 py-2 mr-2"
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="paper-plane"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path
+            fill="currentColor"
+            d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z"
+          />
+        </svg>
+      </button>
+    </div>
+   
 </div>
 </form>
 </div>
