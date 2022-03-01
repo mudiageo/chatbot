130667a8d38d9472d1c74f4318c6ec8310eebf87
mudiageo <76590594+mudiageo@users.noreply.github.com>
2022-03-01 19:55:21 +0100

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,7 @@ let you = ''
 
 let prompt
 onMount(() => {
- prompt =  localStorage.getItem('prompt') || 'You are talking to Mudiaga a web developer with over 8 years of experience in web technologies. He studied at MIT. He is a JavaScript Developer and he works with Sveltekit and Svelte alot.  You: Hello. /n Mudiaga: Hi. /n You: How are you doing today? /n Mudiaga: I am doing alright. /n You: How is MIT like? /n Mudiaga: '
+ prompt = 'You are talking to Mudiaga a web developer with over 8 years of experience in web technologies. He studied at MIT. He is a JavaScript Developer and he works with Sveltekit and Svelte alot.  You: Hello. /n Mudiaga: Hi. /n You: How are you doing today? /n Mudiaga: I am doing alright. /n You: How is MIT like? /n Mudiaga: ' || localStorage.getItem('prompt')
 })
 </script>
 <div class="hero min-h-screen bg-base-200">
@@ -22,7 +22,7 @@ bind:value={you}
         placeholder="Your Name...">
       <br>
 <div class="my-3 text-black-800 font-bold text-lg tracking-wide">
-Prompt: <textarea on:input={this.parentNode.dataset.value = this.value} rows="1" placeholder="Prompt" class="bg-black-800" bind:value={prompt}></textarea></div>
+Prompt: <textarea id="prompt" on:input={document.getElementById("prompt").parentNode.dataset.value = prompt} placeholder="Prompt" class="bg-black-800" bind:value={prompt}></textarea></div>
         
 <br>
       <a href="/{bot}-{you}" on:click={localStorage.setItem('prompt', prompt)} class="btn btn-primary">Get Started</a>
