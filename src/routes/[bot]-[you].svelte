fce82c61aa3cc68a8ad814dd99f9a8cfee7870f8
mudiageo <76590594+mudiageo@users.noreply.github.com>
2022-03-01 19:50:21 +0100

    Update [bot]-[you].svelte

--- src/routes/[bot]-[you].svelte
+++ src/routes/[bot]-[you].svelte
@@ -77,10 +77,10 @@ messages=[]
 <form on:submit|preventDefault={handleText} style="overscroll-behavior: none;">
   <!-- HEADING -->
       <div
-        class="fixed w-full bg-sky-500 h-16 pt-2 text-black flex justify-between shadow-md"
+        class="fixed w-full bg-sky-500 h-16 pt-2 text-white flex justify-between shadow-md"
         style="top:0px; overscroll-behavior: none;"
       >
-        <!-- back button -
+        <!-- back button -->
         <a href="/">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -92,8 +92,8 @@ messages=[]
               d="M9.41 11H17a1 1 0 0 1 0 2H9.41l2.3 2.3a1 1 0 1 1-1.42 1.4l-4-4a1 1 0 0 1 0-1.4l4-4a1 1 0 0 1 1.42 1.4L9.4 11z"
             />
           </svg>
-        </a>-->
-        <div class="my-3 text-black-800 font-bold text-lg tracking-wide">Prompt: <textarea class="bg-black-800" bind:value={prompt}></textarea></div>
+        </a>
+        <div class="my-3 text-sky-100 font-bold text-lg tracking-wide">{$page.params.bot}</div>
         <!-- 3 dots -->
         <div on:click={deleteAllMessages}> Clear
 
