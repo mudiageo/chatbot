5cb6745995f22c768e59e66779a0cb73a21fac9b
Mudia Arha <mudiageo@gmail.com>
2022-03-02 18:58:37 +0100

    new commit

--- src/routes/[bot]-[you].svelte
+++ src/routes/[bot]-[you].svelte
@@ -68,6 +68,9 @@ messages=[]
  localStorage.removeItem("messages")
 }
 }
+const openChat = () => {
+ document.getElementById("chat-section")
+}
 
 </script>
 <svelte:head>
@@ -87,37 +90,14 @@ messages=[]
                 </svg>
               </span>
               <input type="search" class="block w-full py-2 pl-10 bg-gray-100 rounded outline-none" name="search"
-                placeholder="Search" required />
+                placeholder="Search" />
             </div>
           </div>
 
           <ul class="overflow-auto h-[32rem]">
             <h2 class="my-2 mb-2 ml-2 text-lg text-gray-600">Chats</h2>
             <li>
-              <a
-                class="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none">
-                <img class="object-cover w-10 h-10 rounded-full"
-                  src="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg" alt="username" />
-                <div class="w-full pb-2">
-                  <div class="flex justify-between">
-                    <span class="block ml-2 font-semibold text-gray-600">Jhon Don</span>
-                    <span class="block ml-2 text-sm text-gray-600">25 minutes</span>
-                  </div>
-                  <span class="block ml-2 text-sm text-gray-600">bye</span>
-                </div>
-              </a>
-              <a
-                class="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out bg-gray-100 border-b border-gray-300 cursor-pointer focus:outline-none">
-                <img class="object-cover w-10 h-10 rounded-full"
-                  src="https://cdn.pixabay.com/photo/2016/06/15/15/25/loudspeaker-1459128__340.png" alt="username" />
-                <div class="w-full pb-2">
-                  <div class="flex justify-between">
-                    <span class="block ml-2 font-semibold text-gray-600">Same</span>
-                    <span class="block ml-2 text-sm text-gray-600">50 minutes</span>
-                  </div>
-                  <span class="block ml-2 text-sm text-gray-600">Good night</span>
-                </div>
-              </a>
+             
               <a
                 class="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none">
                 <img class="object-cover w-10 h-10 rounded-full"
@@ -127,19 +107,31 @@ messages=[]
                     <span class="block ml-2 font-semibold text-gray-600">Emma</span>
                     <span class="block ml-2 text-sm text-gray-600">6 hour</span>
                   </div>
-                  <span class="block ml-2 text-sm text-gray-600">Good Morning</span>
+                  <span on:click={openChat} class="block ml-2 text-sm text-gray-600">Bot</span>
                 </div>
               </a>
             </li>
           </ul>
         </div>
-        <div class="hidden lg:col-span-2 lg:block">
+        <div id="messages-section" class="hidden lg:col-span-2 lg:block">
           <div class="w-full">
             <div class="relative flex items-center p-3 border-b border-gray-300">
+              <a href="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            class="w-12 h-12 my-1 text-sky-100 ml-2"
+          >
+            <path
+              class="text-sky-100 fill-current"
+              d="M9.41 11H17a1 1 0 0 1 0 2H9.41l2.3 2.3a1 1 0 1 1-1.42 1.4l-4-4a1 1 0 0 1 0-1.4l4-4a1 1 0 0 1 1.42 1.4L9.4 11z"
+            />
+          </svg>
+        </a>
               <img class="object-cover w-10 h-10 rounded-full"
                 src="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg" alt="username" />
               <span class="block ml-2 font-bold text-gray-600">{$page.params.bot}</span>
-              <span class="absolute w-3 h-3 bg-green-600 rounded-full left-10 top-3">
+              <span class="absolute w-3 h-3 bg-green-600 rounded-full left-300 top-3">
               </span>
             </div>
             <div class="relative w-full p-6 overflow-y-auto h-[40rem]">
@@ -194,27 +186,4 @@ messages=[]
           </div>
         </div>
       </div>
-        <a href="/">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            class="w-12 h-12 my-1 text-sky-100 ml-2"
-          >
-            <path
-              class="text-sky-100 fill-current"
-              d="M9.41 11H17a1 1 0 0 1 0 2H9.41l2.3 2.3a1 1 0 1 1-1.42 1.4l-4-4a1 1 0 0 1 0-1.4l4-4a1 1 0 0 1 1.42 1.4L9.4 11z"
-            />
-          </svg>
-        </a>
-       
-        <!-- 3 dots -->
-     
-
-      <!-- MESSAGES -->
-   
-
-       
-
-
-
-</form>
+   </form>
