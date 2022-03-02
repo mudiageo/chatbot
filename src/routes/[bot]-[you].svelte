7adc761d5fb39e3974649a2c53ed9c4a22052d5f
mudiageo <76590594+mudiageo@users.noreply.github.com>
2022-03-02 20:06:00 +0100

    Update [bot]-[you].svelte

--- src/routes/[bot]-[you].svelte
+++ src/routes/[bot]-[you].svelte
@@ -148,7 +148,7 @@ const toggleChat = () => {
               </ul>
             </div>
 <!--Message Input Section-->
-            <div class="flex items-center justify-between w-full p-3 border-t border-gray-300 bottom-0">
+            <div class="flex items-center justify-between w-full p-3 border-t border-gray-300 sticky bottom-0">
               <button>
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24"
                   stroke="currentColor">
