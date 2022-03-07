6d38bb4ea1a3b73a61abcdd3da24588e82752fb4
mudiageo <76590594+mudiageo@users.noreply.github.com>
2022-03-07 09:56:50 +0100

    Update [bot]-[you].svelte

--- src/routes/[bot]-[you].svelte
+++ src/routes/[bot]-[you].svelte
@@ -136,9 +136,9 @@ const toggleChat = () => {
               <img class="object-cover w-10 h-10 rounded-full"
                 src="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg" alt="username" />
               <span class="block ml-2 font-bold text-gray-600">{$page.params.bot}</span>
-              <span class="absolute w-3 h-3 bg-green-600 rounded-full left-0 top-3">
-              </span>
-<span on:click={deleteAllMessages} class="right-0 absolute justify-end px-1"><Icon data={trash}/></span>
+            <!--  <span class="absolute w-3 h-3 bg-green-600 rounded-full left-0 top-3">
+              </span>-->
+<span on:click={deleteAllMessages} class="right-0 absolute justify-end p-3"><Icon data={trash}/></span>
 
             </div>
 
