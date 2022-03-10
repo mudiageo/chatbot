1163db5e37d78d9355010769c06573cb45a6d3bc
mudiageo <76590594+mudiageo@users.noreply.github.com>
2022-03-10 18:13:37 +0100

    Update [bot]-[you].svelte

--- src/routes/[bot]-[you].svelte
+++ src/routes/[bot]-[you].svelte
@@ -321,7 +321,7 @@ const toggleChat = () => {
                     d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z" />
                 </svg>
               </button>
-{:each}
+          {:else}
               <button type="submit">
                 <svg class="w-5 h-5 text-blue-500 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20" fill="currentColor">
