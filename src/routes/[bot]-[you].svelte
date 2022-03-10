add608f64f2edc2497beb424564879b57ff8e72a
mudiageo <76590594+mudiageo@users.noreply.github.com>
2022-03-10 17:43:17 +0100

    Update [bot]-[you].svelte

--- src/routes/[bot]-[you].svelte
+++ src/routes/[bot]-[you].svelte
@@ -245,7 +245,7 @@ const toggleChat = () => {
         </div>
         <div id="messages-section" class="hidden flex flex-col h-screen lg:col-span-2 lg:block">
           <div class="w-full h-full">
-            <div id="normal-menu" class="relative flex items-center p-1 border-b border-gray-300 sticky top-0">
+            <div id="normal-menu" class="relative flex items-center p-1 border-b border-gray-300 sticky top-0 z-30">
               <a on:click={toggleChat}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -265,7 +265,7 @@ const toggleChat = () => {
               </span>-->
 <span on:click={deleteAllMessages} class="right-0 absolute justify-end p-3"><Icon data={trash}/></span>
             </div>
-<div id="selection-menu" class="relative hidden flex items-center p-1 border-b border-sky-500 h-12 text-white bg-sky-500 sticky top-0">
+<div id="selection-menu" class="relative hidden flex items-center p-1 border-b border-sky-500 h-12 text-white bg-sky-500 sticky top-0 z-30">
               <a on:click={cancelSelection}>
           X
         </a>
@@ -303,25 +303,25 @@ const toggleChat = () => {
                     d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
               </button>
-              <button>
+           <!--   <button>
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-500" fill="none" viewBox="0 0 24 24"
                   stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
                 </svg>
-              </button>
+              </button>-->
               <input type="text"
                 class="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700"
                 name="message" on:keyup={localStorage.setItem('newMessage', newMessage)}  bind:value={newMessage} placeholder="Message..."  required />
-              <button>
+              {<button>
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-500" fill="none" viewBox="0 0 24 24"
                   stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z" />
                 </svg>
               </button>
-              <button type="submit" >
-                <svg class="w-5 h-5 text-gray-500 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg"
+              <button type="submit">
+                <svg class="w-5 h-5 text-blue-500 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20" fill="currentColor">
                   <path
                     d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
