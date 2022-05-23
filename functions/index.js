b9ea68768dfeb3d709f804f496f9663c688be989
mudiageo <76590594+mudiageo@users.noreply.github.com>
2022-05-23 02:07:32 +0100

    update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,14 @@
 const functions = require("firebase-functions");
-
+let ssrServerServer;
+                exports.ssrServer = functions.region("us-central1").https.onRequest(async (request, response) => {
+                  if (!ssrServerServer) {
+                    functions.logger.info("Initialising SvelteKit SSR entry");
+                    ssrServerServer = require("./ssrServer/index").default;
+                    functions.logger.info("SvelteKit SSR entry initialised!");
+                  }
+                  functions.logger.info("Requested resource: " + request.originalUrl);
+                  return ssrServerServer(request, response);
+                });
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
