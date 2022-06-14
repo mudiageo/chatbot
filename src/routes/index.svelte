3c94470d75efba33fdbedbceb2387937305f435a
Mudia Arha <mudiageo@gmail.com>
2022-06-14 10:38:54 +0100

    Restructured project, added story generator project, renamed project from chatbot to mudiaga ai

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,23 @@
 <script>
 import {onMount} from 'svelte'
-let bot = ''
-let you = ''
-
 let prompt
 onMount(() => {
- prompt = localStorage.getItem('prompt') || 'You are talking to Mudiaga a web developer with over 8 years of experience in web technologies. He studied at MIT. He is a JavaScript Developer and he works with Sveltekit and Svelte alot.  You: Hello. Mudiaga: Hi. You: How are you doing today? Mudiaga: I am doing alright. You: How is MIT like? Mudiaga: '
+ 
 })
 </script>
 <div class="hero min-h-screen bg-base-200">
   <div class="text-center hero-content">
     <div class="max-w-md">
-      <h1 class="text-5xl font-bold">AI Chat Bot</h1>
+      <h1 class="text-5xl font-bold">Mudiaga's AI Projects</h1>
       <p class="py-6">Input your name, the name of the entity you want to chat with and a prompt with information about the entity. An example prompt is given below in the prompt text area</p>
-     <input class="input"
-bind:value={bot}
-        placeholder="Bot Name...">
-      <br>
+     
 <br>
-       <input class="input"
-bind:value={you}
-        placeholder="Your Name...">
-      <br>
 <div class="my-3 text-black-800 font-bold text-lg tracking-wide">
-Prompt: <textarea id="prompt" on:input={document.getElementById("prompt").parentNode.dataset.value = prompt} placeholder="Prompt" class="bg-black-800" bind:value={prompt}></textarea></div>
-        
+
 <br>
-      <a href="/{bot}-{you}" on:click={localStorage.setItem('prompt', prompt)} class="btn btn-primary">Get Started</a>
+      <a href="/chatbot" class="btn btn-primary">Chatbot</a>
+      <a href="/story-generator" class="btn btn-primary">Story Generator </a>
     </div>
   </div>
+</div>
 </div>
---
+++ src/routes/story-generator/index.svelte
@@ -0,0 +1,46 @@
+<script>
+import {onMount} from 'svelte'
+let prompt
+onMount(() => {
+ prompt = localStorage.getItem('story-prompt') || 'You are talking to Mudiaga a web developer with over 8 years of experience in web technologies. He studied at MIT. He is a JavaScript Developer and he works with Sveltekit and Svelte alot. '
+})
+
+const query = async () => {
+  localStorage.setItem('story-prompt', prompt)
+  const data = {"inputs": prompt}
+  alert(data)
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/EleutherAI/gpt-j-6B",
+		{
+			headers: { Authorization: "Bearer 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" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.json();
+	prompt = result
+alert(prompt)
+}
+
+
+//	console.log(JSON.stringify(response));
+</script>
+
+<div class="hero min-h-screen bg-base-200">
+  <div class="text-center hero-content">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">AI Story Generator</h1>
+      <p class="py-6">Just input the first few lines of your story, and it generates the rest🔥</p>
+     
+<br>
+
+<div class="my-3 text-black-800 font-bold text-lg tracking-wide">
+Prompt: <textarea id="prompt" on:input={document.getElementById("prompt").parentNode.dataset.value = prompt} placeholder="Prompt" class="bg-black-800" bind:value={prompt}></textarea></div>
+        
+<br>
+      <button on:click={query()} class="btn btn-primary">Get Started</button>
+    </div>
+    
+    
+  </div>
+</div>
